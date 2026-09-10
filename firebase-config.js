0ad6cff37643eafd4f7d95adcba6736c6b59815e
AchrafMeh <achrafmeh34@gmail.com>
2026-09-10 12:00:18 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-IlJUAbKBzCsMsqsDnu4j2SaJGosR4Hs",
+  authDomain: "windows-d031e.firebaseapp.com",
+  projectId: "windows-d031e",
+  storageBucket: "windows-d031e.firebasestorage.app",
+  messagingSenderId: "874765961451",
+  appId: "1:874765961451:web:87cb24b031c91744bf56ae",
+  measurementId: "G-RN0DMCG727"
+};
